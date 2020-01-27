bc0e84343a7d1ed2eb2e3e2ba82fb860e371df58
sijab <mar.jablonski96@wp.pl>
2020-01-27 10:25:23 +0100

    edit file

--- PID/pid.c
+++ PID/pid.c
@@ -1,10 +1,3 @@
-/*
- * pid.c
- *
- *  Created on: 06.01.2020
- *      Author: Damian
- */
-
 #include <avr/io.h>
 
 #include "pid.h"
@@ -13,35 +6,23 @@ uint16_t pid(uint16_t setpoint, uint16_t temperature, pid_t *pid_s)
 {
 	int32_t result;
 
-	// calculate regulator error
 	pid_s->errors[pid_s->index & MASK] = setpoint - temperature;
 
-	// calculate derivative term
-		pid_s->derivative = pid_s->errors[pid_s->index & MASK] - pid_s->errors[(pid_s->index-1) & MASK];
+	pid_s->derivative = pid_s->errors[pid_s->index & MASK] - pid_s->errors[(pid_s->index-1) & MASK];
 
-	// if we are far away from setpoint use PD otherwise use full PID function
 	if (pid_s->errors[pid_s->index & MASK] > pid_s->KT || pid_s->errors[pid_s->index & MASK] < -pid_s->KT)
 	{
 		result = (pid_s->KP*pid_s->errors[pid_s->index & MASK] + pid_s->KD*pid_s->derivative);
 	}
 	else
 	{
-		// integrate error
 		pid_s->integral += pid_s->errors[pid_s->index & MASK];
 
-
-		// PID function
 		result = (pid_s->KP*pid_s->errors[pid_s->index & MASK] + (pid_s->KI*pid_s->integral)/100 + pid_s->KD*pid_s->derivative);
 	}
 
 	pid_s->index++;
 
-//	lcd_locate(0,10);
-//	if(result == 300) lcd_cls();
-//	lcd_int(result);
-
-
-	// regulator function bandwidth set to PID_TOP - PID_BOTTOM
 	if (result > PID_TOP)
 	{
 		return (int16_t)PID_TOP;
